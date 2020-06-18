98cf41c45918088eeba64369edd69ff19c8969fc
omarmalonso <omarmalonso@gmail.com>
2020-06-18 08:48:16 -0500

    first commit

---
+++ src/main/java/com/tts/TechTalentTwitter/TechTwitter2Application.java
@@ -0,0 +1,13 @@
+package com.tts.TechTalentTwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechTwitter2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechTwitter2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tts/TechTalentTwitter/TechTwitter2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.TechTalentTwitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechTwitter2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
